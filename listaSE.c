a4ce27e561405b95aa0ab433c4bfd20750bced8a
Icaro Peretti <icaroperetti50@gmail.com>
2020-04-30 18:01:02 -0300

    Update listaSE.c

--- listaSE.c
+++ listaSE.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-    Estrutura de Dados 1
-    Lista Simplesmente Encadeada
-    Aluno: Icaro Peretti
-*/
 
 typedef struct sElemento{
   struct sElemento *next;
